4018c65d915a270cf31ba9858eecc1ac27c73d3e
mawangdan <mawangdan@foxmail.com>
2022-10-07 18:29:12 +0800

    add atomic

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,20 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	nReduce    int
+	files      []string
+	filesIsMap []bool
+	lock       sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -24,6 +29,24 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 
+func (c *Coordinator) callForJob(args *ExampleArgs, reply *ExampleReply) error {
+	reply.Y = args.X + 1
+	return nil
+}
+
+func (c *Coordinator) atomicfilesIsMap() int {
+	tmp := -1
+	c.lock.Lock()
+	for i := 0; i < len(c.filesIsMap); i++ {
+		if !c.filesIsMap[i] {
+			c.filesIsMap[i] = true
+			tmp = i
+			break
+		}
+	}
+	c.lock.Unlock()
+	return tmp
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -50,7 +73,6 @@ func (c *Coordinator) Done() bool {
 
 	// Your code here.
 
-
 	return ret
 }
 
@@ -63,7 +85,8 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
+	c.nReduce = nReduce
+	c.files = files
 
 	c.server()
 	return &c
