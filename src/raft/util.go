d76b25ff695c8120925d567feda06da4a052881b
mawangdan <mawangdan@foxmail.com>
2022-10-12 19:41:04 +0800

    add util

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,13 +1,77 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+	"strconv"
+)
 
 // Debugging
-const Debug = false
+const Debug = true
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
+type LogType uint64
+
+const (
+	LogAESend      LogType = 1
+	LogAERev       LogType = 1 << 1
+	LogRVSend      LogType = 1 << 2
+	LogRVRev       LogType = 1 << 3
+	LogElec        LogType = 1 << 4
+	LogHeartBeat   LogType = 1 << 5
+	LogStateChange LogType = 1 << 6
+)
+
+const LogFlag = LogAESend | LogAERev | LogRVSend | LogRVRev | LogElec | LogHeartBeat | LogStateChange
+
+var LStoStr = map[LogType]string{
+	LogAESend:      "LogAESend",
+	LogAERev:       "LogAERev",
+	LogRVSend:      "LogRVSend",
+	LogRVRev:       "LogRVRev",
+	LogElec:        "LogElec",
+	LogHeartBeat:   "LogHeartBeat",
+	LogStateChange: "LogStateChange",
+}
+
+func (ls LogType) String() string {
+	return LStoStr[ls]
+}
+func DPrintf(lt LogType, perfix string, format string, a ...interface{}) (n int, err error) {
 	if Debug {
-		log.Printf(format, a...)
+		if lt&LogFlag != 0 {
+			log.SetPrefix("[" + perfix + "]")
+			log.Printf(format, a...)
+		}
 	}
 	return
+}
+
+func initLog(file string) {
+	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if err != nil {
+		panic(err)
+	}
+	log.SetOutput(logFile) // 将文件设置为log输出的文件
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
+	return
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func intTo64(i int) int64 {
+	str := strconv.Itoa(i)
+	i64, _ := strconv.ParseInt(str, 10, 64)
+	return i64
 }
