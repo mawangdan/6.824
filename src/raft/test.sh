59bb5ea721dee0e9fb08de5c771a7ca9c34751b9
mawangdan <mawangdan@foxmail.com>
2022-10-16 15:36:53 +0800

    change

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -2,12 +2,13 @@
 
 bk = "FAIL"
 ts="PASS"
-
+i=1
 while true
 do
-    ret = $(rm -rf raft.log && go test -run TestBasicAgree2B  -race)
+    ret=$(rm -rf raft.log && go test -run TestBasicAgree2B  -race)
     if grep -q "$bk" <<< "$ret"; then
     echo "It's there"
     break
     fi
+    i=`expr $i + 1`
 done
